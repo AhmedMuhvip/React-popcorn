5bbd5f30558b617238a9b14f7f704c93d9b30dba
Ahmed <AhmedMuhammed0031@gmail.com>
2024-10-18 20:06:49 +0300

    Improving reusability

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from './App';
 
-import StarRating from './StarRating';
+import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={20} onSetRating={setMovieRating} />
+      <p>This movies was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
-    <StarRating size={24} maxRating={10} color='green' messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      size={24}
+      maxRating={10}
+      color="green"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
     {/* <StarRating maxRating={10} />
     <StarRating /> */}
+
+    <Test />
   </React.StrictMode>
 );
